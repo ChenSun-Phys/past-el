71b2e61caf2492279bddf958e2cee349b232ca76
Chen Sun <sunchen8635@gmail.com>
2022-03-13 12:12:59 +0200

    paste in org mode; module name change; key binding added; minor mode

--- tex-paste-figure.el
+++ tex-paste-figure.el
@@ -1,46 +1,27 @@
 ;; -*- lexical-binding: t -*-
-;;; tex-paste.el --- paste images into a LaTeX document using xclip
+;;; past.el --- paste images into a LaTeX document using xclip
 
 ;;; Commentary:
-;; You can insert a new figure at point using tex-paste-figure
+;; You can insert a new figure at point using pastel-figure
 ;; it is partly motivated by ink.el (https://github.com/foxfriday/ink)
 ;; TODO: Edit an existing figure with tex-edit-figure.
 
 ;;; Code:
 
 
-(defvar tex-paste-fig-dir "drawings"
+;;
+;; the tex strings
+;;
+(defvar pastel-fig-dir "drawings"
   "Default image directory.")
 
-(defun tex-paste-insert-fig (file)
-  "Insert tex string associated with FILE."
-  (let* ((fdir_full (expand-file-name tex-paste-fig-dir default-directory))
-	 (fdir (file-relative-name fdir_full default-directory))
-         (dname (file-name-directory file))
-         (fname (file-name-nondirectory file))
-         (name (file-name-sans-extension fname))
-         (caption (downcase name))
-         (ltex (format tex-paste-figure-string fdir name caption))
-	 (ltex_graph (format tex-paste-graphics-string fdir name)))
-    (message dname)
-    (message (format "current-prefix-arg: %s" current-prefix-arg))
-    (if current-prefix-arg
-	(insert ltex_graph)
-      (insert ltex))
-    )
-  )
+(makunbound 'pastel-graphics-string-tex)
+(makunbound 'pastel-figure-string-tex)
 
-;; check
-;; (eval (format tex-paste-graphics-string "1" "2"))
-;; (eval (format tex-paste-figure-string "1" "2" "3"))
-
-(makunbound 'tex-paste-graphics-string)
-(makunbound 'tex-paste-figure-string)
-
-(defvar tex-paste-graphics-string "\n    \\includegraphics[width=.8\\textwidth]{%s/%s.png}"
+(defvar pastel-graphics-string-tex "\n    \\includegraphics[width=.8\\textwidth]{%s/%s.png}"
   "Default latex insert for the graphics. Used inside a figure environment")
 
-(defvar tex-paste-figure-string "\n\\begin{figure}[ht]
+(defvar pastel-figure-string-tex "\n\\begin{figure}[ht]
     \\centering
     \\includegraphics[width=.8\\textwidth]{%s/%s.png}
     \\label{fig:%s}
@@ -48,15 +29,79 @@
 \\end{figure}\n"
   "Default latex insert for the full figure.")
 
-;; simple test of elisp
+(defvar pastel-figure-string-org "[[%s/%s][%s]]"
+  "Default org insert for the figure.")
+
+;; debug checks
+;; (eval (format pastel-graphics-string-tex "1" "2"))
+;; (eval (format pastel-figure-string-tex "1" "2" "3"))
 ;; (let ((test (file-name-sans-extension "abc.test")))
 ;;   (message test))
 
-(defun tex-paste-figure (fig)
+
+;;
+;; the main routine
+;;
+(defun pastel-insert-fig-tex (file)
+  "Insert file into tex with includegraphics. If prefix is nil, it is wrapped by the figure environment."
+  (let* ((fdir_full (expand-file-name pastel-fig-dir default-directory))
+	 (fdir (file-relative-name fdir_full default-directory))
+         (dname (file-name-directory file))
+         (fname (file-name-nondirectory file))
+         (name (file-name-sans-extension fname))
+         (caption (downcase name))
+         (ltex (format pastel-figure-string-tex fdir name caption))
+	 (ltex_graph (format pastel-graphics-string-tex fdir name)))
+    (message dname)
+    (message (format "current-prefix-arg: %s" current-prefix-arg))
+    ;; (insert ltex)
+    (if current-prefix-arg
+    	(insert ltex_graph)
+      (insert ltex))
+    )
+  )
+
+
+(defun pastel-insert-fig-org (file)
+  "Insert file into org"
+  ;; (let* (())
+  ;;   (message dname)
+  ;;   (message (format "current-prefix-arg: %s" current-prefix-arg))
+  ;;   ;; (insert ltex)
+  ;;   (if current-prefix-arg
+  ;;   	(insert ltex_graph)
+  ;;     (insert ltex))
+  ;;   )
+  (let* ((fdir_full (expand-file-name pastel-fig-dir default-directory))
+	 (fdir (file-relative-name fdir_full default-directory))
+         (dname (file-name-directory file))
+         (fname (file-name-nondirectory file))
+         (name (file-name-sans-extension fname))
+         (caption (downcase name))
+         (orgstr (format pastel-figure-string-org
+			 (concat "./" fdir) fname caption)))
+    (message dname)
+    (insert orgstr)
+    )
+  )
+
+
+;;
+;; sentinels for pastel-figure
+;;
+(defun pastel-sentinel (process event)
+  (when (memq (princ major-mode) '(org-mode))
+      (org-display-inline-images))
+  )
+
+;;
+;; the user interface
+;;
+(defun pastel-figure (fig)
   "Make a new figure named FIG and insert it at point."
   (interactive "sFigure name: ")
-  (let* ((log-buffer (get-buffer-create "*tex-paste-log*"))
-         (tdir (expand-file-name tex-paste-fig-dir default-directory))
+  (let* ((log-buffer (get-buffer-create "*pastel-log*"))
+         (tdir (expand-file-name pastel-fig-dir default-directory))
          (file (concat (file-name-as-directory tdir) fig ".png"))
 	 )
     (print file)
@@ -69,47 +114,29 @@
     		  :filter (lambda (_proc chunk)
     			    (let* ((coding-system-for-write 'raw-text))
     			      (write-region chunk nil file 'append 'silent)))		  
-                  :stderr log-buffer)
-                  ;; :sentinel 'ink-sentinel)		  
+                  :stderr log-buffer
+                  :sentinel 'pastel-sentinel)		  
 		  ;; :connection-type 'pipe
 		  ;; :stderr (make-pipe-process :name "dd test null" :filter #'ignore))		  
-    (tex-paste-insert-fig file)
+    (when (memq (princ major-mode) '(org-mode))
+      (pastel-insert-fig-org file))
+    (when (memq (princ major-mode) '(latex-mode))
+      (pastel-insert-fig-tex file))    
     )
   )
 
 
 
-;; alternatively, one could output everything to a buffer and save the buffer afterward. 
 
-
-;; select region of screen
-;; this part is motivated by desktop-environment.el
-
-;; (makunbound 'pastel-screenshot-partial-command)
-;; (defcustom pastel-screenshot-partial-command "scrot -s -e 'printf $f'"
-;;   "Shell command taking a partial screenshot in the current working directory.
-
-;; The shell command should let the user interactively select the
-;; portion of the screen."
-;;   :type 'string)
-
-;; (defun pastel-sentinel (process event)
-;;   "Wait for inkscape PROCESS to close but has no use for EVENT."
-;;   (when (memq (process-status process) '(exit signal))
-;;     ;; TODO: how to pass fig to sentinel?    
-;;     (tex-paste-figure figlocal)
-;;     )
-;;   )
-
-(defun pastel-screenshot-part (fig)
+(defun pastel-screenshot-part ()
   "Take a partial screenshot in the current working directory.
 
 The command asks the user to interactively select a portion of
 the screen."
-  ;; (interactive)
-  (interactive "sFigure name: ")
-  (let* ((log-buffer (get-buffer-create "*tex-paste-log*"))
-	 (fdir_full (expand-file-name tex-paste-fig-dir default-directory))
+  (interactive)
+  ;; (interactive "sFigure name: ")
+  (let* ((log-buffer (get-buffer-create "*pastel-log*"))
+	 (fdir_full (expand-file-name pastel-fig-dir default-directory))
 	 (default-directory fdir_full)
 	 ;; (figlocal fig)
 	 )
@@ -118,10 +145,9 @@ the screen."
     (make-process :name "pastel-scrot"
     		  :coding 'raw-text		  
                   :command (list "scrot" "/tmp/%F_%T_$wx$h.png" "-s" "-e" "xclip -selection clipboard -target image/png -i $f")
-                  ;; :command (list "scrot" "/tmp/%F_%T_$wx$h.png" "-e" "trash-put $f")		  
 		  :sentinel (lambda (process event)
-			      (when (memq (process-status process) '(exit signal))
-				(tex-paste-figure fig)))
+		  	      (when (memq (process-status process) '(exit signal))
+		  		(call-interactively 'pastel-figure)))
                   :buffer log-buffer
                   :stderr log-buffer		  
 		  )
@@ -129,28 +155,51 @@ the screen."
     )
   )
 
-;; TODO: deine a minor mode and keybinding
 
-;; (define-minor-mode desktop-environment-mode
-;;   "Activate keybindings to control your desktop environment.
 
-;; \\{desktop-environment-mode-map}"
-;;   :global t
-;;   :require 'desktop-environment
-;;   :lighter " DE"
-;;   (desktop-environment-exwm-set-global-keybindings desktop-environment-mode))
 
-;; TODO: input
-;; take file absolute path, (later paste copies it to the drawing folder)
+;;
+;; keybindings
+;;
+
+(makunbound 'pastel-mode-map)
+(defvar pastel-mode-map
+  (let ((pastel--keybindings
+         `(;; Brightness
+           (,(kbd "H-y") . ,(function pastel-figure))
+	   (,(kbd "H-o") . ,(function pastel-screenshot-part))))
+        (map (make-sparse-keymap)))
+    (dolist (keybinding pastel--keybindings)
+      (define-key map (car keybinding) (cdr keybinding)))
+    map)
+  "Keymap for `pastel-mode'.")
+
+(define-minor-mode pastel-mode
+  "Activate keybindings to control your desktop environment.
+
+\\{desktop-environment-mode-map}"
+  :global nil
+  :require 'pastel
+  :lighter " PA"
+  :keymap (pastel-mode-map))
+
+(provide 'pastel)
+
+
+;; DONE: deine a minor mode and keybinding
+
+;; input
+;; TODO take file absolute path, (later paste copies it to the drawing folder)
 ;; DONE take screen region (saves to drawing folder)
+;; DONE take xournalpp handwriting copy
 
-;; TODO: output
-;; org-mode insert
-;; latex insert (done)
+;; output
+;; DONE: org-mode insert
+;; DONE latex insert 
 
-;; TODO: change module name to pastel
+;; DONE: change module name to pastel
 
-;; TODO: keybinding H-i H-y 
+;; DONE: keybinding H-o H-y 
 
 ;; TODO: if supplied with a file name use file name, otherwise time stamp
 
